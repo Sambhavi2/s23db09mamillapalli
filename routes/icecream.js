cb6a2cc12ae68820f60a44508a8aa774b1d5efb4
Durga Sambhavi <S564609@nwmissouri.edu>
2023-11-08 21:26:36 -0600

    added

--- routes/icecream.js
+++ routes/icecream.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('icecream', { title: 'Search Results Ice-Cream' });
 });
 
 module.exports = router;
