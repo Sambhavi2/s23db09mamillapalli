139ac0051c9641c8d809c9976767daf06cf22058
Durga Sambhavi <S564609@nwmissouri.edu>
2023-11-08 23:36:46 -0600

    finalcommit

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -11,6 +11,27 @@ exports.icecream_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle icecream create on POST.
+    exports.icecream_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"icecream_type":"goat", "cost":12, "size":"large"}
+    document.flavour = req.body.flavour;
+    document.size = req.body.size;
+    document.price = req.body.price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
   // VIEWS
 // Handle a show all view
 exports.icecream_view_all_Page = async function(req, res) {
@@ -32,9 +53,9 @@ exports.icecream_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream detail: ' + req.params.id);
 };
 // Handle icecream create on POST.
-exports.icecream_create_post = function(req, res) {
+/*exports.icecream_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream create POST');
-};
+};*/
 // Handle icecream delete form on DELETE.
 exports.icecream_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id);
