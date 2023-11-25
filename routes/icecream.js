edfc201bc44ef9a5688890e2047aa3783ac3e155
Durga Sambhavi <S564609@nwmissouri.edu>
2023-11-25 17:41:52 -0600

    ss4

--- routes/icecream.js
+++ routes/icecream.js
@@ -3,14 +3,22 @@ const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
 
 router.get('/', icecream_controlers.icecream_view_all_Page);
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}   
+/* GET create update page */
+router.get('/update',secured, icecream_controlers.icecream_update_Page);
 // GET request for one icecream.
 router.get('/icecream/:id', icecream_controlers.icecream_detail);
 /* GET detail icecream page */
 router.get('/detail', icecream_controlers.icecream_view_one_Page);
 /* GET create icecream page */
 router.get('/create', icecream_controlers.icecream_create_Page);
-/* GET create update page */
-router.get('/update', icecream_controlers.icecream_update_Page);
+
 /* GET delete icecream page */
 router.get('/delete', icecream_controlers.icecream_delete_Page);
 
