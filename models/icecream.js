45c09588ede12bfb8021db5ea5f8e2fccde4ff47
Durga Sambhavi <S564609@nwmissouri.edu>
2023-11-29 18:32:45 -0600

    bonus

--- models/icecream.js
+++ models/icecream.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
-flavour: String,
-size: String,
-price: Number
+flavour: {type:String, minlength:2,maxlength:20},
+size: {type:String, minlength:3,maxlength:20},
+price: {type:Number, min:2,max:10}
 })
 module.exports = mongoose.model("Icecreame",icecreamSchema)
--- routes/icecream.js
+++ routes/icecream.js
@@ -2,7 +2,8 @@ var express = require('express');
 const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
 
-router.get('/', icecream_controlers.icecream_view_all_Page);
+router.get('/', icecream_controlers.icecream_view_all_Page );
+
 const secured = (req, res, next) => {
     if (req.user){
     return next();
@@ -12,14 +13,14 @@ const secured = (req, res, next) => {
 }   
 /* GET create update page */
 router.get('/update',secured, icecream_controlers.icecream_update_Page);
-// GET request for one icecream.
-router.get('/icecream/:id', icecream_controlers.icecream_detail);
 /* GET detail icecream page */
-router.get('/detail', icecream_controlers.icecream_view_one_Page);
+router.get('/detail',icecream_controlers.icecream_view_one_Page);
+
 /* GET create icecream page */
-router.get('/create', icecream_controlers.icecream_create_Page);
+router.get('/create',secured, icecream_controlers.icecream_create_Page);
+
 
 /* GET delete icecream page */
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
 
 module.exports = router;
